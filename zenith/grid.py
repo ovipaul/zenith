c905ce276b5ff4f5ae53732e970f7a717569c6b0
ovipaul <ovipaulcs@gmail.com>
2020-07-31 14:57:22 +0600

    quality update 0.2

--- zenith/grid.py
+++ zenith/grid.py
@@ -1,23 +1,23 @@
-from PIL import Image 
-import numpy as np
 import os
+from PIL import Image
+import numpy as np
 
 def grid(img_file, img_dim, overlap_percentage):
-    
     if not os.path.exists('grid_output'):
         os.makedirs('grid_output')
 
     overlap_percentage = 100-overlap_percentage
     file_name = img_file.rstrip('.png')
-    img = Image.open(img_file) 
+    img = Image.open(img_file)
 
     cal_img = np.array(img)
+
     rows, cols  = cal_img.shape[0:2]
 
     overlap = int((img_dim*overlap_percentage)/100)#img_width
 
-    # Setting the points for cropped image 
-    top = 0    
+    # Setting the points for cropped image
+    top = 0
     bottom = img_dim #img_height
 
     counter = 0
@@ -26,7 +26,6 @@ def grid(img_file, img_dim, overlap_percentage):
     col = 0
 
     for row in range(rows):
-
         if (rows-bottom) < img_dim : #img_height
             break
         #initial vs other time values
@@ -35,17 +34,15 @@ def grid(img_file, img_dim, overlap_percentage):
             bottom = bottom+overlap
 
         left = 0
-        right = img_dim           #img_width
-        
+        right = img_dim     #img_width
         for col in range(cols):
-
             if (cols-right) < img_dim : #img_width
                 break
             if col != 0:
                 left = left+overlap
                 right = right+overlap
 
-            final_img = img.crop((left, top, right, bottom)) 
+            final_img = img.crop((left, top, right, bottom))
             final_img.save('grid_output'+'/'+file_name+'_'+str(counter)+'.png')
             counter = counter + 1
 
