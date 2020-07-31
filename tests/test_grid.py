8390246a5d80bfbc96bdb2a3e3b4124f2fdab865
ovipaul <ovipaulcs@gmail.com>
2020-07-31 14:42:30 +0600

    quality update 0.1

--- tests/test_grid.py
+++ tests/test_grid.py
@@ -1,26 +1,15 @@
 from PIL import Image
-import glob
-import numpy as np
-# from zenith.grid import grid
-import io
-Image.MAX_IMAGE_PIXELS = 350000000000000
-
-
-# Opens a image in RGB mode 
 from zenith.grid import grid
 
-import pytest
+Image.MAX_IMAGE_PIXELS = 350000000000000
 
 def test_grid():
-    
-    output_directory = 'output'
 
     overlap_percentage = 50
-    img_width = 250
-    img_height = 250
+    img_dim = 250
     file_name = 'test_image.png'
     print(file_name)
-    number_of_grid = grid(file_name,img_width,img_height,overlap_percentage,output_directory)
+    number_of_grid = grid(file_name, img_dim, overlap_percentage)
 
     assert number_of_grid == 81
 
--- zenith/grid.py
+++ zenith/grid.py
@@ -1,35 +1,33 @@
-# Improting Image class from PIL module 
 from PIL import Image 
 import numpy as np
-# Opens a image in RGB mode 
-import io
+import os
 
-def grid(img_file,img_width,img_height,overlap_percentage,output_directory):
+def grid(img_file, img_dim, overlap_percentage):
+    
+    if not os.path.exists('grid_output'):
+        os.makedirs('grid_output')
+
     overlap_percentage = 100-overlap_percentage
     file_name = img_file.rstrip('.png')
-
-
     img = Image.open(img_file) 
-    # with open(img_file, 'rb') as f:
-    #     img = Image.open(io.BytesIO(f.read()))
 
     cal_img = np.array(img)
-    rows, cols, cha = cal_img.shape
+    rows, cols  = cal_img.shape[0:2]
 
+    overlap = int((img_dim*overlap_percentage)/100)#img_width
 
-    overlap = int((img_width*overlap_percentage)/100)
     # Setting the points for cropped image 
-
     top = 0    
-    bottom = img_height
-
+    bottom = img_dim #img_height
 
     counter = 0
     #initial coordinate
     row = 0
     col = 0
+
     for row in range(rows):
-        if (rows-bottom) < img_height :
+
+        if (rows-bottom) < img_dim : #img_height
             break
         #initial vs other time values
         if row != 0:
@@ -37,20 +35,18 @@ def grid(img_file,img_width,img_height,overlap_percentage,output_directory):
             bottom = bottom+overlap
 
         left = 0
-        right = img_width
+        right = img_dim           #img_width
         
         for col in range(cols):
-            if (cols-right) < img_width :
+
+            if (cols-right) < img_dim : #img_width
                 break
             if col != 0:
                 left = left+overlap
                 right = right+overlap
 
-            
-
             final_img = img.crop((left, top, right, bottom)) 
-
-
-            final_img.save(output_directory+'/'+file_name+'_'+str(counter)+'.png')
+            final_img.save('grid_output'+'/'+file_name+'_'+str(counter)+'.png')
             counter = counter + 1
+
     return counter
