38498e52dd0ad7f909a477838ac6be129cce68a1
Ovi Paul <ovipaulcs@gmail.com>
2020-07-31 18:06:23 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="zenith", 
-    version="0.0.1",
+    version="0.0.2",
     author="Ovi Paul",
     author_email="ovipaulcs@gmail.com",
     description="A library with various functionality that can be useful in the processing of data in machine learning, computer vision, data science etc.",
